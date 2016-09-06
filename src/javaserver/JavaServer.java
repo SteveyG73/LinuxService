d8742a25e52806b2c27db0e7dc668b1e8ddd510c
Steve <Steve@DEEP-THOUGHT3>
2016-09-07 00:47:07 +0100

    Tweaks to start command

--- src/javaserver/JavaServer.java
+++ src/javaserver/JavaServer.java
@@ -5,31 +5,42 @@
  */
 package javaserver;
 
-
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 import java.net.InetSocketAddress;
 
 import com.sun.net.httpserver.HttpServer;
+
 /**
  *
  * @author Steve
  */
 public class JavaServer {
 
-    
     private static final Logger LOG = LogManager.getRootLogger();
+    private static HttpServer server;
+
     /**
      * @param args the command line arguments
      * @throws java.lang.Exception
      */
     public static void main(String[] args) throws Exception {
+
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                LOG.info("Shutting down server");
+                server.stop(5);
+                LOG.info("Shutdown complete");
+            }
+        });
+
         LOG.info("Starting server on port 8181");
-        HttpServer server = HttpServer.create(new InetSocketAddress("localhost",8181), 0);
+        server = HttpServer.create(new InetSocketAddress(8181), 0);
         server.createContext("/test", new MyHandler());
         server.setExecutor(null); // creates a default executor
-        LOG.info("Access server via http://"+server.getAddress().getAddress().getCanonicalHostName()+":"+server.getAddress().getPort()+"/test");
+        LOG.info("Access server via http://" + server.getAddress().getAddress().getCanonicalHostName() + ":" + server.getAddress().getPort() + "/test");
         server.start();
     }
-    
+
 }
